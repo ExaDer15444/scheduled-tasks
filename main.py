65b4125d8f5382651c24a251e95615906c497e71
ExaDer15444 <130552796+ExaDer15444@users.noreply.github.com>
2026-08-28 15:55:58 +0200

    Refactor email sending and weather analysis logic

--- main.py
+++ main.py
@@ -1,38 +1,125 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+import smtplib
+import ssl
+from email.mime.text import MIMEText
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = os.environ.get("OWM_API_KEY")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+my_email = os.environ.get("WP_EMAIL")
+PASSWORD = os.environ.get("WP_PASSWORD")
+to_email = os.environ.get("TO_EMAIL")
+
+
+
+
+
+def analyze_day(codes):
+    result = {
+        "storm": any(200 <= c <= 232 for c in codes),
+        "rain": any(300 <= c <= 531 for c in codes),
+        "snow": any(600 <= c <= 622 for c in codes),
+        "sun": any(c == 800 for c in codes),
+        "clouds": any(801 <= c <= 804 for c in codes),
+    }
+    return result
+
+def message_for_asia(codes):
+
+    weather = analyze_day(codes)
+
+    if weather["storm"]:
+        return (
+            "Kochana Asiu ❤️ Dzisiaj może pojawić się burza. "
+            "Pamiętaj jednak, że niezależnie od tego, co dzieje się za oknem, "
+            "jesteś moim najjaśniejszym promieniem słońca. ☀️"
         )
+
+    if weather["rain"]:
+        if weather["sun"]:
+            return (
+                "Kochana Asiu ❤️ Dzień zapowiada się trochę deszczowo, "
+                "ale między chmurami powinno pokazać się też słoneczko. "
+                "Mam nadzieję, że Twój uśmiech rozgoni wszystkie chmurki. Kocham Cię! ☀️🥰"
+            )
+
+        return (
+            "Kochana Asiu ❤️ Dzisiaj może popadać, ale pamiętaj, "
+            "że nawet najładniejszy deszcz nie jest tak piękny jak Twój uśmiech. 🌷"
+        )
+
+    if weather["sun"] and not weather["clouds"]:
+        return (
+            "Kochana Asiu ❤️ Zapowiada się piękny, słoneczny dzień. "
+            "Mam nadzieję, że będzie dla Ciebie tak ciepły i cudowny jak Ty dla mnie. ☀️😘"
+        )
+
+    if weather["sun"] and weather["clouds"]:
+        return (
+            "Kochana Asiu ❤️ Dzień przyniesie trochę słońca i trochę chmurek, "
+            "ale jedno jest pewne: dla mnie zawsze jesteś najpiękniejszą pogodą. ☀️💖"
+        )
+
+    return (
+        "Kochana Asiu ❤️ Dzień zapowiada się spokojnie. "
+        "Życzę Ci mnóstwa powodów do uśmiechu i cudownego nastroju. 🥰"
+    )
+
+weather_params = {
+    "lat": 54.42880315,
+    "lon": 18.798325902846855,
+    "appid": API_KEY,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+#print(weather_data["list"][0]["weather"][0]["description"])
+
+
+condition_codes =[]
+
+for hour_data in weather_data["list"]:
+    condition_codes.append(hour_data["weather"][0]["id"])
+print(condition_codes)
+
+print(message_for_asia(condition_codes))
+message_for_asia = message_for_asia(condition_codes)
+
+
+message = MIMEText(
+    message_for_asia,
+    "plain",
+    "utf-8"
+)
+
+message["From"] = my_email
+message["To"] = to_email
+message["Subject"] = "Specjalna prognoza pogody dla żonki"
+
+context = ssl.create_default_context()
+
+print("1. Łączenie...")
+with smtplib.SMTP_SSL("smtp.wp.pl", 465, context=context) as connection:
+    print("2. Połączono")
+
+    connection.login(
+        user=my_email,
+        password=PASSWORD
+    )
+
+    print("3. Zalogowano")
+
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=to_email,
+        msg=message.as_string()
+    )
+
+    print("4. Email wysłany")
+
